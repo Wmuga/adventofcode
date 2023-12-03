be177f1405f2382ea94aefc101c8788e7009fd3d
Wmuga <dsasadf13@gmail.com>
2023-12-03 22:45:37 +0300

    day1

---
+++ 2023/days/day1/day1.go
@@ -0,0 +1,90 @@
+package day1
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+type Day1 struct {
+	inputLines []string
+}
+
+var strToNumbers = map[string]string{
+	"one":   "o1e",
+	"two":   "t2o",
+	"three": "t3e",
+	"four":  "f4r",
+	"five":  "f5e",
+	"six":   "s6x",
+	"seven": "s7n",
+	"eight": "e8t",
+	"nine":  "n9e",
+}
+
+func New(input string) *Day1 {
+	lines := strings.Split(input, "\n")
+	return &Day1{
+		inputLines: lines,
+	}
+}
+
+func (d *Day1) baseSolver(deb bool, filter func([]string) []string) int {
+	out := 0
+	filtered := filter(d.inputLines)
+	for _, line := range filtered {
+		if len(line) == 0 {
+			continue
+		}
+		runes := []rune(line)
+		numbStr := string(runes[0]) + string(runes[len(runes)-1])
+		numb, err := strconv.ParseInt(numbStr, 10, 32)
+		if err != nil {
+			fmt.Printf("%v is not a number\n", numbStr)
+			continue
+		}
+		if deb {
+			fmt.Print(numb, ", ")
+		}
+		out += int(numb)
+	}
+	if deb {
+		fmt.Println()
+	}
+	return out
+}
+
+func (d *Day1) SolveA(deb bool) {
+	out := d.baseSolver(deb, filterNumbersA)
+	fmt.Println("Solution A:", out)
+}
+
+func (d *Day1) SolveB(deb bool) {
+	out := d.baseSolver(deb, filterNumbersB)
+	fmt.Println("Solution B:", out)
+}
+
+func filterNumbersA(inp []string) []string {
+	out := make([]string, len(inp))
+	for i, v := range inp {
+		out[i] = ""
+		for _, c := range v {
+			if unicode.IsDigit(c) {
+				out[i] += string(c)
+			}
+		}
+	}
+	return out
+}
+
+func filterNumbersB(inp []string) []string {
+	inp2 := make([]string, len(inp))
+	copy(inp2, inp)
+	for k, v := range strToNumbers {
+		for i, line := range inp2 {
+			inp2[i] = strings.ReplaceAll(line, k, v)
+		}
+	}
+	return filterNumbersA(inp2)
+}
--- 2023/main.go
+++ 2023/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"aoc2023/days/day1"
 	"aoc2023/utils/inputs"
 	"fmt"
 	"log"
@@ -13,15 +14,32 @@ type AoCDay interface {
 	SolveB(bool)
 }
 
+const inpTest = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`
+
 func main() {
+	var curDay AoCDay
+
 	if godotenv.Load() != nil {
 		log.Fatalln("Can't load env")
 	}
 
-	inp, err := inputs.Day(1)
+	inpActual, err := inputs.Day(1)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	fmt.Println(inp)
+	fmt.Println("Test data:")
+	curDay = day1.New(inpTest)
+	curDay.SolveA(true)
+	curDay.SolveB(true)
+	fmt.Println("Actual data:")
+	curDay = day1.New(inpActual)
+	curDay.SolveA(false)
+	curDay.SolveB(false)
 }
